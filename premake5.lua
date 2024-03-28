08709120d68b7d895effacf9a15c063a081c3681
trianglxboi <trianglxboi@gmail.com>
2024-03-28 23:50:37 +0300

    Build Script and Project Files

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "Glad"
+	language "C"
+	staticruntime "Off"
+	kind "StaticLib"
+
+	targetdir "%{OutputDir.Binaries}/%{prj.name}"
+	objdir    "%{OutputDir.Objects}/%{prj.name}"
+
+	files
+	{
+		"include/*.h",
+		"src/*.c"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Development"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Preview or Shipping"
+		runtime "Release"
+		optimize "Speed"
